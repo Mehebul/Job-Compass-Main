1dae412ffb671fd46cff797a773231b963a6ebac
Mehebul Sarkar <mehebulsarkar@gmail.com>
2024-03-20 18:09:36 +0530

    updated login

--- Job-Compass-Client/src/Router/Router.jsx
+++ Job-Compass-Client/src/Router/Router.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home";
-import Login from "../auth/Login";
+// import Login from "../auth/Login";
 import Error from "../Pages/Error";
-// import Register from './../auth/Register';
+import Register from './../auth/Register';
 
 
 
@@ -13,8 +13,9 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/Login", element: <Login /> },
-      { path: "/PageNotFound", element: <Error /> }
+      // { path: "/Login", element: <Login /> },
+      { path: "/Register", element: <Register /> },
+      { path: "*", element: <Error /> }
   ],
   },
 ]);
